0b83709964ee2f241b22a9c342b2f3ce2d6d254c
DavidOrtegaGarcia <36156063+DavidOrtegaGarcia@users.noreply.github.com>
2018-03-11 20:43:23 +0100

    Create ejercicio-bucle-sumar.py

---
+++ ejercicio-bucle-sumar.py
@@ -0,0 +1,18 @@
+# #coding: utf8
+#David Ortega Garcia
+#11/03/2018
+
+# Inicializaciones
+salir = "N"
+numero=1
+resultado=0
+while ( salir=="N" ):
+	# Hago cosas
+	print (resultado)
+
+	# Incremento
+	numero=numero+1
+	resultado=resultado+numero
+	# Activo indicador de salida si toca
+	if (resultado=15 ): # Condición de salida
+    	    salir = "S"
