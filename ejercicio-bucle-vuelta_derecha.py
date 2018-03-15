d3fe2a473a3d41da0bbd957938c3d9ff9057e91c
DavidOrtegaGarcia <36156063+DavidOrtegaGarcia@users.noreply.github.com>
2018-03-15 17:03:50 +0100

    Update ejercicio-bucle-vuelta_derecha.py

--- ejercicio-bucle-vuelta_derecha.py
+++ ejercicio-bucle-vuelta_derecha.py
@@ -8,16 +8,16 @@ contador=1
 
 while ( salir=="N" ):
 	  # Hago cosas
-	  if ((contador==1) or(contador==2)):
-        print ("Arriba")
+    if ((contador==1) or(contador==2)):
+        print ('Arriba')
     if ((contador==3) or(contador==4)):
-        print ("Derecha")
+        print ('Derecha')
     if ((contador==5) or(contador==6)):
-        print ("Abajo")
+        print ('Abajo')
     if ((contador==7) or(contador==8)):
-        print ("Izquierda")
-	  # Incremento
-	  contador=contador+1
-	  # Activo indicador de salida si toca
-	  if (contador>8): # Condición de salida
-    	    salir = "S"
+        print ('Izquierda')
+    # Incremento
+    contador=contador+1
+    # Activo indicador de salida si toca
+    if (contador>8): # Condición de salida
+        salir = "S"
